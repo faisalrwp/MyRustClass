7139f1368da4e54c49afd3d4155eb4489f439867
Faisal Shahzad <faisal.rwp@gmail.com>
2020-01-05 20:20:00 -0500

    20191013

---
+++ 20191013/hello/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+    let mut num1=100;
+    let num2=200;
+    println!("NUM 1 = {} AND NUM 2 = {}", num1, num2);
+    num1=num2;
+    println!("NUM 1 = {} AND NUM 2 = {}", num1, num2);
+}
